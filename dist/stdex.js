9dfa0cece09264e14c54963c42a34c8aea4953dc
Max Korsakov <Maksim_Karsakou1epam.com>
2021-03-13 15:04:30 +0300

    fix comments

--- dist/stdex.js
+++ dist/stdex.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var stdin = process.openStdin();
+var stdin = process.stdin;
 stdin.addListener("data", function (d) {
   process.stdout.write(d.toString().trim().split("").reverse().join(""));
-  process.exit();
+  process.stdout.write('\n');
 });
--- src/stdex.js
+++ src/stdex.js
@@ -1,6 +1,6 @@
-let stdin = process.openStdin();
+let stdin = process.stdin;
 
 stdin.addListener("data", (d) => {
-    process.stdout.write( d.toString().trim().split("").reverse().join(""))
-    process.exit();
+    process.stdout.write( d.toString().trim().split("").reverse().join(""));
+    process.stdout.write('\n');
 })
